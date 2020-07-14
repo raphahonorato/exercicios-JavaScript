a20ab4df58667057bb5f86be601db2f930c7957b
Raphael Honorato <raphaahonorato@gmail.com>
2020-07-14 03:19:20 -0300

    do while

--- ex-015.js
+++ ex-015.js
@@ -1,5 +1,14 @@
-var v = 1
-while (v <= 500){
-    console.log(`Vou conseguir nessa porra ${v} !`)
-    v++
-}
+/*var v = 1;
+while (v <= 15) {
+  console.log(`Vou conseguir nessa porra ${v} !`)
+  v++
+} 
+console.log('Vc é pica !')
+*/
+
+var v = 1;
+do{
+  console.log(`Vou conseguir nessa porra ${v} !`)
+  v++
+}while (v <= 15)  
+console.log('Vc é pica !')
