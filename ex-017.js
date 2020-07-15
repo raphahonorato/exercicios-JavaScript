d3ae81f04a3e221822f10f353539d0a58286edc9
Raphael Honorato <raphaahonorato@gmail.com>
2020-07-15 14:59:50 -0300

    arrays

---
+++ ex-017.js
@@ -0,0 +1,20 @@
+let a = [2,4,6]
+/*
+for(let pos in a){
+    console.log(`No indice ${pos}, esta o elemento ${a[pos]} `)
+}
+*/
+
+let pos = a.indexOf(2)
+if(pos == -1){
+    console.log ('ERRO')
+}else{
+    console.log(`O elemento ${a[pos]} , esta no indice ${pos} !`)
+}
+
+/*
+for(pos = 0; pos < a.length ; pos++){
+    console.log (`No indice ${pos}, esta o elemento ${a[pos]} `)
+}
+*/
+
